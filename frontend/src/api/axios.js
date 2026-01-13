15e9ff6f3e5774266ebcbaa3df50e4c4a315f51f
vishaku03 <vishalk.shrivastav1@gmail.com>
2026-01-13 15:49:53 +0530

    issues

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,12 +1,16 @@
+
+
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://lost-found-site-2y0q.onrender.com/api/auth/login"
+  baseURL: "https://lost-found-site-2y0q.onrender.com", // ✅ ONLY base URL
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
